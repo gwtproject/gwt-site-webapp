edae80d15f913e47063a3dbe5a0195a028cded50
Manuel Carrasco Moñino <manuel.carrasco.m@gmail.com>
2013-05-21 16:02:58 +0200

    Simply code: - Improve sameOrigin regexp. - Protocol should be considered when checking same origin XHR. - Return false instead of calling stopPropagation & preventDefault. - Use gQuery "or" method when getting anchor href. - Use gQuery toggleClass method instead of if/else block. - Formating: replacing tab with space
    Change-Id: Idd8f779601d301c8fa67dc0d05e7f30de611ba79

--- src/main/java/com/google/gwt/site/webapp/client/GWTProjectEntryPoint.java
+++ src/main/java/com/google/gwt/site/webapp/client/GWTProjectEntryPoint.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Daniel Kurka
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,162 +15,146 @@
  */
 package com.google.gwt.site.webapp.client;
 
+import static com.google.gwt.query.client.GQuery.*;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
+import com.google.gwt.query.client.js.JsUtils;
 import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
 
-import static com.google.gwt.query.client.GQuery.$;
-import static com.google.gwt.query.client.GQuery.body;
-
 public class GWTProjectEntryPoint implements EntryPoint {
-	private static final RegExp INTERNAL_URL_REGEX;
-	private static final RegExp NOT_RELATIVE_URL_REGEX;
 
-	static {
-		String domainUrl = Window.Location.getHostName();
-		String port = Window.Location.getPort();
+  private static final RegExp isSameOriginRexp;
 
-		if (port != null && port.length() > 0) {
-			domainUrl += ":" + port;
-		}
+  static {
+    // XHR must match all: protocol, host and port.
+    // Note: in chrome this could be simpler since it has window.location.origin
+    String origin = Window.Location.getProtocol() + "//" +  Window.Location.getHostName();
+    String port = Window.Location.getPort();
+    if (port != null && port.length() > 0) {
+      origin += ":" + port;
+    }
+    // We discard links with a different origin, and hash links.
+    isSameOriginRexp = RegExp.compile("^" + origin + "|^(?!(https?|mailto|ftp|javascript):|#).+", "i");
+  }
 
-		INTERNAL_URL_REGEX = RegExp.compile("^(https?)\\:\\/\\/" + domainUrl
-				+ "\\/.*$");
-		NOT_RELATIVE_URL_REGEX = RegExp
-				.compile("^(https?)|(mailto)|(ftp)|(javascript)\\:.*$");
-	}
+  private String currentPage;
 
-	private String currentPage;
+  @Override
+  public void onModuleLoad() {
+    enhanceMenu();
 
-	@Override
-	public void onModuleLoad() {
-		enhanceMenu();
+    openMenu(Window.Location.getPath());
 
-		openMenu(Window.Location.getPath());
+    if (supportsHtml5History()) {
+      initOnPopState();
+    }
+  }
 
-		if (supportsHtml5History()) {
-			initOnPopState();
-		}
-	}
+  private void openMenu(String path) {
+    $("#gwt-toc a.selected").removeClass("selected");
 
-	private void openMenu(String path) {
-		$("#gwt-toc a.selected").removeClass("selected");
+    if (path != null && path.length() > 0) {
+      $("#gwt-toc a[ahref$='" + path + "']")
+        .addClass("selected")
+        .parentsUntil("#gwt-toc")
+        .filter("li.folder")
+        .addClass("open")
+        .children("ul")
+        .slideDown(200);
+    }
+  }
 
-		if (path != null && path.length() > 0) {
-			$("#gwt-toc a[ahref$='" + path + "']").addClass("selected")
-					.parentsUntil("#gwt-toc").filter("li.folder")
-					.addClass("open").children("ul").slideDown(200);
-		}
-	}
+  private void enhanceMenu() {
+    $("li.folder > a").click(new Function() {
+      @Override
+      public boolean f(Event e) {
+        toggleMenu($(e).parent());
+        return false;
+      }
+    });
 
-	private void enhanceMenu() {
-		// TODO : arrow should be clickable
-		$("li.folder > a").click(new Function() {
-			@Override
-			public void f(Element e) {
-				toggleMenu($(e).parent());
+    $("#gwt-toc li.folder > ul").hide();
 
-				getEvent().stopPropagation();
-				getEvent().preventDefault();
-			}
-		});
+    $("a", body).live(Event.ONCLICK, new Function() {
+      @Override
+      public boolean f(Event e) {
+        // Sometimes the link src comes in the 'ahref' attribute
+        String href = JsUtils.or($(e).attr("ahref"), $(e).attr("href"));
 
-		$("#gwt-toc li.folder > ul").css("display", "none");
+        // if loadPage returns true we don't stop propagation nor default
+        return loadPage(href, true);
+      }
+    });
+  }
 
-		$("a", body).live(Event.ONCLICK, new Function() {
-			@Override
-			public boolean f(Event event) {
-				String href = null;
-				if (getElement().hasAttribute("ahref")) {
-					href = getElement().getAttribute("ahref");
-				} else {
-					href = getElement().getAttribute("href");
-				}
+  private void toggleMenu(GQuery menu) {
+    menu.toggleClass("open")
+        .children("ul")
+        .slideToggle(200);
+  }
 
-				if (isInternalNavigation(href)) {
-					return loadPage(href, true);
-				}
+  private boolean loadPage(final String pageUrl, final boolean pushState) {
 
-				return true;
-			}
-		});
-	}
+    if (!isSameOriginRexp.test(pageUrl)) {
+      return true;
+    }
 
-	private boolean isInternalNavigation(String href) {
-		// TODO mix the three last conditions in one regex !!
-		return href != null
-				&& href.length() > 0
-				&& (!href.startsWith("#") && !NOT_RELATIVE_URL_REGEX.test(href) || INTERNAL_URL_REGEX
-						.test(href));
-	}
+    int hashIndex = pageUrl.indexOf('#');
+    final String path = hashIndex != -1 ? pageUrl.substring(0, hashIndex)
+        : pageUrl;
+    final String hash = hashIndex != -1 ? pageUrl.substring(hashIndex)
+        : null;
 
-	private void toggleMenu(GQuery menu) {
-		if (menu.hasClass("open")) {
-			menu.removeClass("open");
-		} else {
-			menu.addClass("open");
-		}
+    if (supportsHtml5History() && !path.equals(currentPage)) {
+      currentPage = path;
 
-		menu.children("ul").slideToggle(200);
-	}
+      if (pushState) {
+          pushState(pageUrl);
+      }
 
-	private boolean loadPage(final String pageUrl, final boolean pushState) {
-		int hashIndex = pageUrl.indexOf('#');
-		final String path = hashIndex != -1 ? pageUrl.substring(0, hashIndex)
-				: pageUrl;
-		final String hash = hashIndex != -1 ? pageUrl.substring(hashIndex)
-				: null;
-
-		if (supportsHtml5History() && !path.equals(currentPage)) {
-			currentPage = path;
-
-            if (pushState) {
-                pushState(pageUrl);
+      $("#gwt-content").load(pageUrl + " #gwt-content > div", null,
+          new Function() {
+            @Override
+            public void f() {
+              if (hash != null) {
+                scrollTo(hash);
+              }
+              openMenu(path);
             }
+          });
 
-			$("#gwt-content").load(pageUrl + " #gwt-content > div", null,
-					new Function() {
-						@Override
-						public void f() {
-							if (hash != null) {
-								scrollTo(hash);
-							}
-							openMenu(path);
-						}
-					});
+      return false;
+    } else if (hash != null) {
+      scrollTo(hash);
+    }
 
-			return false;
-		} else if (hash != null) {
-			scrollTo(hash);
-		}
+    return !path.equals(currentPage);
+  }
 
-		return !path.equals(currentPage);
-	}
+  private void scrollTo(String hash) {
+    Window.scrollTo(Window.getScrollLeft(), $(hash).offset().top);
+  }
 
-	private void scrollTo(String hash) {
-		Window.scrollTo(Window.getScrollLeft(), $(hash).offset().top);
-	}
+  private void onPopState() {
+    loadPage(Window.Location.getPath(), false);
+  }
 
-	private void onPopState() {
-		loadPage(Window.Location.getPath(), false);
-	}
+  private native void pushState(String url) /*-{
+    $wnd.history.pushState(null, null, url);
+  }-*/;
 
-	private native void pushState(String url) /*-{
-		$wnd.history.pushState(null, null, url);
-	}-*/;
+  private native boolean supportsHtml5History()/*-{
+    return !!($wnd.history && $wnd.history.pushState);
+  }-*/;
 
-	private native boolean supportsHtml5History()/*-{
-		return !!($wnd.history && $wnd.history.pushState);
-	}-*/;
-
-	private native void initOnPopState() /*-{
-		var that = this;
-		$wnd.onpopstate = $entry(function(e) {
-			that.@com.google.gwt.site.webapp.client.GWTProjectEntryPoint::onPopState()();
-		});
-	}-*/;
+  private native void initOnPopState() /*-{
+    var that = this;
+    $wnd.onpopstate = $entry(function(e) {
+      that.@com.google.gwt.site.webapp.client.GWTProjectEntryPoint::onPopState()();
+    });
+  }-*/;
 }
