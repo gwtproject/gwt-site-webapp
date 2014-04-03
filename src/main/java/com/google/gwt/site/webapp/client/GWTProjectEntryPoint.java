c1e26aa217c6a634f2150d81cb16d2f6fec6f169
jdramaix <julien.dramaix@gmail.com>
2014-04-03 14:00:58 +0200

    Fix issue when user try to open a link in a new tab/window.
    Do not load the content of page by ajax when user try to open the page in a new tab/window by clicking in the middle/right button of the mouse or by using crtl-click/cmd-click/shift-click
    
    Bug: issue 8265
    Change-Id: Ic65dbfdb054be9eb9e3cd1fbd21ae01fa7acc6b5

--- src/main/java/com/google/gwt/site/webapp/client/GWTProjectEntryPoint.java
+++ src/main/java/com/google/gwt/site/webapp/client/GWTProjectEntryPoint.java
@@ -16,6 +16,7 @@
 package com.google.gwt.site.webapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
 import com.google.gwt.query.client.Properties;
@@ -23,6 +24,7 @@ import com.google.gwt.query.client.js.JsUtils;
 import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.impl.HyperlinkImpl;
 
 import static com.google.gwt.query.client.GQuery.$;
 import static com.google.gwt.query.client.GQuery.body;
@@ -31,8 +33,10 @@ import static com.google.gwt.query.client.GQuery.window;
 public class GWTProjectEntryPoint implements EntryPoint {
 
   private static final RegExp isSameOriginRexp;
+  private static final HyperlinkImpl clickHelper = GWT.create(HyperlinkImpl.class);
   private static Properties history = JsUtils.prop(window, "history");
   private static boolean isPushstateCapable = history.get("pushState") != null;
+
   static {
     // XHR must match all: protocol, host and port.
     // Note: in chrome this could be simpler since it has window.location.origin
@@ -44,6 +48,7 @@ public class GWTProjectEntryPoint implements EntryPoint {
     // We discard links with a different origin, hash links and protocol-agnostic urls.
     isSameOriginRexp = RegExp.compile("^" + origin + "|^(?!(https?|mailto|ftp|javascript):|#|//).+", "i");
   }
+
   private String currentPage;
 
   @Override
@@ -85,27 +90,42 @@ public class GWTProjectEntryPoint implements EntryPoint {
     $("a", body).live(Event.ONCLICK, new Function() {
       @Override
       public boolean f(Event e) {
-        // do not load links that are marked as full page reload
-        String async = $(e).attr("data-full-load");
-        if("true".equals(async)) {
-          return true;
-        }
-
-        // Sometimes the link src comes in the 'ahref' attribute
-        String href = JsUtils.or($(e).attr("ahref"), $(e).attr("href"));
-
-        //do not load javadoc async
-        if(href.startsWith("/javadoc/")) {
-          return true;
-        }
-
-        if (isPushstateCapable && isSameOriginRexp.test(href)) {
+        String href = getHref($(e));
+        if (shouldEnhanceClick(e) && isPushstateCapable && isSameOriginRexp.test(href)) {
           loadPage(href);
           return false;
         }
         return true;
       }
     });
+  }
+
+  private boolean shouldEnhanceClick(Event e) {
+    GQuery link = $(e);
+
+    // Is it a normal click (not ctrl/cmd/shift/right/middle click) ?
+    if (!clickHelper.handleAsClick(e)) {
+      return false;
+    }
+
+    // do not load links that are marked as full page reload
+    if ("true".equals(link.attr("data-full-load"))) {
+      return false;
+    }
+
+    // do not load javadoc async
+    if (getHref(link).startsWith("/javadoc/")) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Sometimes the link src comes in the 'ahref' attribute
+   */
+  private String getHref(GQuery link) {
+    return  JsUtils.or(link.attr("ahref"), link.attr("href"));
   }
 
   private void toggleMenu(GQuery menu) {
